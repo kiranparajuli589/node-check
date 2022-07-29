0ec08c1d39b6c53677cf0bea0b47cd4b4dd88dc5
PKiran <39373750+kiranparajuli589@users.noreply.github.com>
2022-07-29 10:50:56 +0545

    Add info for the targetted issue (#1)
    * Add info for the targetted issue
    
    Signed-off-by: Kiran Parajuli <kiranparajuli589@gmail.com>
    
    * Fix github folder name
    
    Signed-off-by: Kiran Parajuli <kiranparajuli589@gmail.com>
    
    * Add branch name as string
    
    Signed-off-by: Kiran Parajuli <kiranparajuli589@gmail.com>
    
    * Fix lint in the workflow
    
    Signed-off-by: Kiran Parajuli <kiranparajuli589@gmail.com>
    
    * Fix styfle image
    
    Signed-off-by: Kiran Parajuli <kiranparajuli589@gmail.com>

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const a = "Kiran Parajuli"
+
+/**
+ * @description: The replaceAll function is only available from node v15
+ * we're currently using node v14.x for the project, see package.json -> engines
+ * but still the linter is not complaining about the usage of replaceAll
+ */
 console.log(a.replaceAll(" ", "_"))
